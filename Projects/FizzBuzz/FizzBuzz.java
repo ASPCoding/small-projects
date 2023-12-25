7706af4384cee33734beacb6b3db35ca2dea40ea
ASPCoding <152766272+ASPCoding@users.noreply.github.com>
2023-12-24 17:59:25 -0800

    Added FizzBuzz

---
+++ Projects/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class FizzBuzz{
+    public static void main(String[] args) throws Exception{
+        
+        Scanner num = new Scanner(System.in);
+
+        System.out.println("Enter a number.");
+
+        int endnum = num.nextInt();
+
+        for (int i = 1; i <= endnum; i++){
+            if(i % 15 == 0){
+                System.out.println("Fizzbuzz");
+            }
+            else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }
+            else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+    }
+}
